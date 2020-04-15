15fab4364341a614f39493293b5d1ca5eee865f3
tanveer3567 <shaiktanveer.ahmed18@gmail.com>
2020-04-15 03:35:56 -0500

    added resume

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ function footer() {
         <MDBFooter color="blue" className="font-small pt-4 mt-4">
           <div className="footer-copyright text-center py-3" bg="dark" variant="dark" expand="lg">
             <MDBContainer fluid>
-              &copy; {new Date().getFullYear()} Copyright: Tanveer 
+            Developed using React &copy;  {new Date().getFullYear()} Copyright: Tanveer 
             </MDBContainer>
           </div>
         </MDBFooter>
